bda535e260c06fc706a8162b5bb38068ab0b7e6c
alicelandroe <alice@landroe.com>
2020-07-29 10:04:59 +0200

    finished social stats (mobile)
    started on new members area

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // TRAFFIC GRAPH
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+const trafficCanvas = document.getElementById('traffic-chart').getContext('2d');
+const myChart = new Chart(trafficCanvas, {
     type: 'line',
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
         "4-10", "11-17", "18-24", "25-31"],
         datasets: [{
-            label: 'traffic',
+            label: '',
             data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
                 2500],
             backgroundColor: [
@@ -16,10 +16,21 @@ var myChart = new Chart(ctx, {
                 'rgba(127,134,202, 1)'
             ],
             borderWidth: 1,
-            pointRadius: 5
+            pointRadius: 5,
+            pointBorderWidth: 2,
+            pointBorderColor: 'rgba(127,134,202, 1)',
+            pointBackgroundColor: 'rgb(255,255,255)',
+            lineTension: 0
         }]
     },
     options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: 'Traffic',
+        },
         scales: {
             yAxes: [{
                 ticks: {
@@ -27,5 +38,63 @@ var myChart = new Chart(ctx, {
                 }
             }]
         }
+    }
+});
+
+// DAILY TRAFFIC CHART
+
+const dailyCanvas = document.getElementById('dailyCanvas').getContext('2d');
+const dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            label: '# of Hits',
+            data: [75, 115, 175, 125, 225, 200, 100],
+            backgroundColor: '#7477BF',
+            borderWidth: 1
+            }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        title: {
+            display: true,
+            text: 'Daily traffic',
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+// MOBILE USERS CHART
+
+const mobileUsers = document.getElementById('mobile-users').getContext('2d');
+const mobileChart = new Chart(mobileUsers, {
+    type: 'doughnut',
+    data: {
+        labels:  ["Desktop", "Tablet", "Phones"],
+        datasets: [{
+            label: '# of Users',
+            data: [2000, 550, 500],
+            borderWidth: 0,
+            backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+            ]
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Mobile',
+        },
     }
 });
